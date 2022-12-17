8ae32a8203e43c1872400923f08e7fbb0f93e11e
sisira s <sisiramohan2000@gmail.com>
2022-12-17 20:56:18 +0530

    first commit

---
+++ distance to inches.py
@@ -0,0 +1,4 @@
+
+distance_feet=int(input("enter the distance:"))
+distance_inches=distance_feet*12
+print("The distance in mile:",distance_inches)
---
+++ percentage of student.py
@@ -0,0 +1,22 @@
+#python program to area and perimeter of the circle
+NAME=str(input("enter your name:"))
+Sub_1=float(input("enter the mark of the CProgramming:"))
+Sub_2=float(input("enter the mark of the DataStructure:"))
+Sub_3=float(input("enter the mark of the DigitalElectronic:"))
+Sub_4=float(input("enter the mark of the WebProgramming :"))
+Sub_5=float(input("enter the mark of the SoftwareEngineering :"))
+Sub_6=float(input("enter the mark of the MatheMatics:"))
+#1200 is the maximum mark obtained by student
+maximum_mark=1200
+#calculate the total mark
+mark_obtained =Sub_1+Sub_2+Sub_3+Sub_4+Sub_5+Sub_6
+#calculate the percentage
+percentage=(mark_obtained/1200)*100
+print("your total mark is:",mark_obtained)
+print("your percentage is:",percentage)
+
+
+
+
+
+
