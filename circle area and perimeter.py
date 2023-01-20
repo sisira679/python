28738ce21fa0b863a45db313a5087fc930065b62
sisira679 <120799302+sisira679@users.noreply.github.com>
2023-01-20 06:57:06 +0530

    Add files via upload

---
+++ circle area and perimeter.py
@@ -0,0 +1,9 @@
+#python program to area and perimeter of the circle
+r=int(input("enter the radius of circle:"))
+pi=3.14
+#calculate the area of the circle
+area=3.14*r*r
+#calculate the perimeter of the circle
+peri=2*3.14*r
+print("\tthe area of circle:",area)
+print('\tthe perimeter of circle:',peri)
