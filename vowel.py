0755c69f67cbdfd0cc6743f5a3fe284b416b38b6
sisira679 <120799302+sisira679@users.noreply.github.com>
2023-01-20 06:51:38 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,7 @@
+sta=input("enter any statement:")
+vowels=['a','e','i','o','u']
+list1=[]
+for x in sta:
+    if(x in vowels and x not in list1):
+        list1.append(x)
+        print("vowels present in given statement:",list1)
