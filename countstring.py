7cd2dc351ae43e496355b68a81ae1ef1c2b5269d
sisira679 <120799302+sisira679@users.noreply.github.com>
2023-01-20 06:54:38 +0530

    Add files via upload

---
+++ countstring.py
@@ -0,0 +1,10 @@
+string=str(input("Enter string:"))
+word=str(input("Enter word:"))
+a=[]
+count=0
+a=string.split(" ")
+for i in range(0,len(a)):
+      if(word==a[i]):
+            count=count+1
+print("Count of the word is:")
+print(count)
